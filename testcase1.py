331cade9c32a38a164b0f42c9bb3ebc329db1abb
Asif-Asees <140640064+Asif-Asees@users.noreply.github.com>
2024-06-03 16:38:16 +0530

    Update testcase1.py

--- testcase1.py
+++ testcase1.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Read the text file into a DataFrame
-text_df = spark.read.text("/home/asif/data/spark/vechicle.txt")
+text_df = spark.read.text("vechicle.txt")
 
 # Split each line into words and count the occurrences of each word
 word_counts = text_df.select(explode(split(text_df.value, " ")).alias("word")) \
