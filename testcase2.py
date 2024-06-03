ece1580c934c91949278d7660e195d26fdfba5fb
Asif-Asees <140640064+Asif-Asees@users.noreply.github.com>
2024-06-03 16:24:54 +0530

    Add files via upload

---
+++ testcase2.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split, count
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("count2") \
+    .config("spark.sql.repl.eagerEval.enabled", True) \
+    .getOrCreate()
+
+# Read the text file into a DataFrame
+text_df = spark.read.text("/home/asif/data/spark/veh.txt")
+
+# Split each line into words and count the occurrences of each word
+word_counts = text_df.select(explode(split(text_df.value, " ")).alias("word")) \
+                     .groupBy("word") \
+                     .agg(count("*").alias("count"))
+
+# Show the word counts without truncation
+word_counts.show(n=100)
+
+# Stop the SparkSession
+spark.stop()
+
+
